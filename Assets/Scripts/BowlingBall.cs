59ff662b6bbbdba3e87429d19128bec7622fbd2d
Jacob McGowan <73966737+TheDevAtlas@users.noreply.github.com>
2024-08-19 17:21:09 -0400

    Bowling Ball Throw + Basic Inside Out Traking

---
+++ Assets/Scripts/BowlingBall.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using WiiU = UnityEngine.WiiU;
+
+public class BowlingBall : MonoBehaviour {
+	
+	// 0 Is Left, 1 Is Right //
+	public int channel;
+
+	public GameObject bowlingBall;
+	public Transform armHold;
+
+	WiiU.Remote remote;
+	WiiU.MotionPlus motionPlus;
+
+	public float speed;
+	Transform startTransform;
+
+	void Start()
+	{
+		remote = WiiU.Remote.Access (0);
+		motionPlus = remote.motionPlus;
+
+		motionPlus.Enable (WiiU.MotionPlusMode.Standard);
+
+		startTransform = bowlingBall.transform;
+	}
+
+	void Update()
+	{
+		WiiU.RemoteState state = remote.state;
+
+		if(state.IsTriggered(WiiU.RemoteButton.A))
+		{
+			bowlingBall.transform.SetParent (armHold);
+			bowlingBall.transform.rotation = startTransform.rotation;
+			bowlingBall.transform.position = Vector3.zero;
+			bowlingBall.GetComponent<Rigidbody> ().isKinematic = true;
+		}
+
+		if (state.IsReleased (WiiU.RemoteButton.B))
+		{
+			bowlingBall.transform.SetParent (null);
+
+			Rigidbody rb = bowlingBall.GetComponent<Rigidbody> ();
+			rb.isKinematic = false;
+
+			rb.AddForce (transform.forward * speed);
+		}
+	}
+}
